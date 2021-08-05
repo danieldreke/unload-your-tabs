86808172416805fb5f7506a7a8c83f1e3dc0bb58
Daniel D <22680274+danieldreke@users.noreply.github.com>
2021-08-05 23:04:08 +0530

    show/hide list of unloadable tabs

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ async function addAndActivateBlankTab(window) {
     var isBlankTab = tab.url === 'about:newtab';
     if (isBlankTab) {
       hasBlankTab = true;
-      browser.tabs.update(tab.id, { active: true });
+      await browser.tabs.update(tab.id, { active: true });
       break;
     }
   }
   if (!hasBlankTab) {
-    browser.tabs.create({ windowId: window.id, active: true });
+    await browser.tabs.create({ windowId: window.id, active: true });
   }
 }
 
@@ -40,7 +40,6 @@ async function unloadAllTabsExceptActiveTab() {
   var tabIds = tabs.map((tab) => tab.id);
   var tabIdsFiltered = tabIds.filter((tabId) => tabId != activeTab.id);
   await browser.tabs.discard(tabIdsFiltered);
-  window.close();
 }
 
 async function unloadAllTabsExceptActiveWindow() {
@@ -48,9 +47,122 @@ async function unloadAllTabsExceptActiveWindow() {
   var nonCurrentWindowTabs = await browser.tabs.query({ currentWindow: false });
   var tabIds = nonCurrentWindowTabs.map((tab) => tab.id);
   await browser.tabs.discard(tabIds);
-  window.close();
+}
+
+async function unloadTab(e) {
+  var tabLink = e.currentTarget;
+  if (tabLink.tab.active) {
+    await addAndActivateBlankTab(tabLink.window);
+  }
+  await browser.tabs.discard(tabLink.tabId);
+}
+
+async function unloadWindowTabs(e) {
+  var windowLink = e.currentTarget;
+  var window = windowLink.window;
+  var tabs = await browser.tabs.query({ windowId: window.id, discarded: false });
+  var tabIds = tabs.map((tab) => tab.id);
+  await addAndActivateBlankTab(window);
+  await browser.tabs.discard(tabIds);
+}
+
+function isAboutTab(tab) {
+  var result = tab.url.startsWith('about');
+  return result;
+}
+
+async function showUnloadableTabs() {
+  var tabList = document.getElementById('tab-list');
+  tabList.textContent = '';
+  var windows = await browser.windows.getAll({ populate: true });
+  var windowNr = 1;
+  for (var window of windows) {
+    var tabCount = 0;
+    for (var tab of window.tabs) {
+      var isTabDiscardable = !tab.discarded && !isAboutTab(tab);
+      if (isTabDiscardable) {
+        tabCount++;
+        // create window link
+        if (tabCount == 1) {
+          let windowLink = document.createElement('div');
+          windowLink.setAttribute('id', `window${window.id}`);
+          windowLink.textContent = `Unload Window ${windowNr}`;
+          windowLink.classList.add('link');
+          windowLink.classList.add('bold');
+          windowLink.window = window;
+          windowLink.addEventListener('click', unloadWindowTabs);
+          tabList.appendChild(windowLink);
+        }
+        // create tab link
+        let tabLink = document.createElement('div');
+        tabLink.setAttribute('id', `tab${tab.id}`);
+        tabLink.classList.add('link');
+        tabLink.classList.add('tabinfo');
+        tabLink.tabId = tab.id;
+        tabLink.window = window;
+        tabLink.tab = tab;
+        tabLink.addEventListener('click', unloadTab);
+
+        let tabIcon = document.createElement('img');
+        tabIcon.setAttribute('id', `icon${tab.id}`);
+        tabIcon.setAttribute('class', 'favicon');
+        tabIcon.setAttribute('src', tab.favIconUrl);
+        tabLink.appendChild(tabIcon);
+
+        let tabTitle = document.createElement('span');
+        tabTitle.textContent = `${tab.title}`;
+        tabLink.appendChild(tabTitle);
+
+        tabList.appendChild(tabLink);
+      }
+    }
+    windowNr++;
+  }
+}
+
+async function toggleTabList() {
+  let toggleLink = document.getElementById('toggle-tab-list');
+  if (toggleLink.textContent.includes('Show')) {
+    toggleLink.textContent = toggleLink.textContent.replace('Show', 'Hide');
+    await showUnloadableTabs();
+  }
+  else {
+    toggleLink.textContent = toggleLink.textContent.replace('Hide', 'Show');
+  }
+  let tabList = document.getElementById('tab-list');
+  tabList.classList.toggle('display-none');
+}
+
+async function hasUnloadableTabs(window) {
+  var _hasUnloadableTabs = false;
+  var tabs = await browser.tabs.query({ windowId: window.id, discarded: false });
+  for (var tab of tabs) {
+    if (!isAboutTab(tab)) {
+      _hasUnloadableTabs = true;
+    }
+  }
+  return _hasUnloadableTabs;
+}
+
+function removeWindowLink(windowId) {
+  var windowLink = document.getElementById(`window${windowId}`);
+  windowLink.remove();
+}
+
+async function removeTabLink(eventTabId) {
+  var tabLink = document.getElementById(`tab${eventTabId}`);
+  var window = tabLink.window;
+  tabLink.remove();
+  var _hasUnloadableTabs = await hasUnloadableTabs(window);
+  if (!_hasUnloadableTabs) {
+    removeWindowLink(window.id);
+  }
 }
 
 document.getElementById('unload-all-tabs').addEventListener('click', unloadAllTabs);
 document.getElementById('keep-active-tab').addEventListener('click', unloadAllTabsExceptActiveTab);
 document.getElementById('keep-active-window').addEventListener('click', unloadAllTabsExceptActiveWindow);
+document.getElementById('toggle-tab-list').addEventListener('click', toggleTabList);
+
+browser.tabs.onUpdated.addListener(removeTabLink, { properties: ['discarded'] });
+//browser.tabs.onUpdated.addListener(showUnloadableTabs, { properties: ['status'] });
